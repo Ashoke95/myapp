94ac1cee4cfc1c518b17993231e5ca460391927f
FalmingosDev <89380775+FalmingosDev@users.noreply.github.com>
2022-06-09 10:43:11 +0530

    first commit

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+
+  private url =
+  'http://jsonplaceholder.typicode.com/posts';
+  
+  constructor(private httpClient: HttpClient) { }
+
+  getPosts(){
+    return this.httpClient.get(this.url);
+    }
+   
+    create(post: any){
+    return this.httpClient.post(this.url,
+   JSON.stringify(post));
+    }
+}
